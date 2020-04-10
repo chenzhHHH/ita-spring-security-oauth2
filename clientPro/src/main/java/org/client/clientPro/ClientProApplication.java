fedc6676719fc31b108049ca56fd0f4218e70480
Eric Chen <eric.z.h.chen@oocl.com>
2020-04-10 10:16:31 +0800

    init project

---
+++ clientPro/src/main/java/org/client/clientPro/ClientProApplication.java
@@ -0,0 +1,13 @@
+package org.client.clientPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientProApplication.class, args);
+	}
+
+}
---
+++ clientPro/src/test/java/org/client/clientPro/ClientProApplicationTests.java
@@ -0,0 +1,13 @@
+package org.client.clientPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
