e74d9c00f8489fb39144e4e6b1413efbbdf860d3
Eric Chen <eric.z.h.chen@oocl.com>
2020-04-12 18:05:44 +0800

    init project for security

---
+++ security/src/test/java/com/ita/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ita.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
