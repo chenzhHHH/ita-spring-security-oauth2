2f6d14f65f0d58449e4e689fcb8fd41754c94e45
Eric Chen <eric.z.h.chen@oocl.com>
2020-04-21 12:58:03 +0800

    need to fix resourceServer problem

--- security/src/main/java/com/ita/security/config/ResourceServerConfiguration.java
+++ security/src/main/java/com/ita/security/config/ResourceServerConfiguration.java
@@ -1,64 +1,64 @@
-//package com.ita.security.config;
-//
-//
-//import com.ita.security.service.MyUserDetailsService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-//import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-//import org.springframework.security.oauth2.provider.token.TokenStore;
-//
-//@Configuration
-//@EnableResourceServer
-//public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-//
-//    private static final String RESOURCE_ID = "res1";
-//
-//    @Autowired
-//    private TokenStore tokenStore;
-//    @Autowired
-//    private MyUserDetailsService myUserDetailsService;
-//    @Autowired
-//    private AuthenticationProvider authenticationProvider;
-//
-//    @Bean
-//    @Primary
-//    public DefaultTokenServices tokenService(){
-//        DefaultTokenServices service=new DefaultTokenServices();
-//        service.setTokenStore(tokenStore);
-//        service.setSupportRefreshToken(true);
-//        return service;
-//    }
-//
-//    @Override
-//    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-//        resources.resourceId(RESOURCE_ID)//资源Id
-//                .tokenServices(tokenService())//验证令牌的服务
-//                .stateless(true);
-//    }
-//
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception {
-//        http
-//                .cors()
-//                .and()
-//                .csrf()
-//                    .disable()
-//                .formLogin()
-//                .and()
-//                .authorizeRequests()
-//                    .antMatchers("/user/resource").access("#oauth2.hasScope('all')")
-//                    .anyRequest().permitAll()
-//                .and()
-//                .authenticationProvider(authenticationProvider)
-//                .sessionManagement()
-//                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//    }
-//}
+package com.ita.security.config;
+
+
+import com.ita.security.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    private static final String RESOURCE_ID = "res1";
+
+    @Autowired
+    private TokenStore tokenStore;
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenService(){
+        DefaultTokenServices service=new DefaultTokenServices();
+        service.setTokenStore(tokenStore);
+        service.setSupportRefreshToken(true);
+        return service;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId(RESOURCE_ID)//资源Id
+                .tokenServices(tokenService())//验证令牌的服务
+                .stateless(true);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                    .disable()
+                .formLogin()
+                .and()
+                .authorizeRequests()
+                    .antMatchers("/user/resource").access("#oauth2.hasScope('all')")
+                    .anyRequest().permitAll()
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
